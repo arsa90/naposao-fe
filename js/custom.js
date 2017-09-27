12003eb8c44c03aee34e1721e176f3f3baa77002
arsa <arsenije018@gmail.com>
2017-09-27 11:20:20 +0200

    Authorized person for contact company registration
    CSS, JS, HTML

--- js/custom.js
+++ js/custom.js
@@ -9,6 +9,26 @@ $(document).ready(function(){
 		$('.search_job_filters_holder').slideToggle(200);
 	});
 
+	// Check PIB Company - Show Second Part Of Form
+	$('.btn_check_company_pib').on('click', function(e){
+		e.preventDefault();
+		$('.first_part_companies_reg').toggleClass('none');
+		$('.second_part_companies_reg').toggleClass('none');
+	});
+
+	// Check 
+
+
+
+	$('input:radio[name="authorized_person"]').change(
+    function(){
+        if (this.checked && this.value == 'No') {
+            $('.authorized_person_for_contact_company').removeClass('none');
+        } else {
+        	$('.authorized_person_for_contact_company').addClass('none');
+        }
+    });
+
 	// Business sector form selection
 	$("#business_sector_select option").click(function () {           
 	  	var selectedVal = $(this).text();
